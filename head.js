de2bbd086d5094295a9b2004afcabbf890212e16
Shivam.rungta <shivam.rungta@IN-Shivamrungta.local>
2019-05-09 14:42:06 +0530

    Comments added in head.js file

--- head.js
+++ head.js
@@ -11,27 +11,35 @@ try{
     if(process.argv[2].match(reg)){
         //if option is with line
         if(process.argv[2][1] == 'n'){
+            //if option is used with space between specifying line/byte
             if(process.argv[2].length > 2){
+                //single file
                 if(process.argv.length == 4){
                     printfilelines(process.argv[3], process.argv[2].split('n')[1]);
                 }
+                //multiple file
                 else{
                     for(let i = 3;i < process.argv.length;i++){
                         console.log("==> "+ process.argv[i] +".txt <==")
                         printfilelines(process.argv[i], process.argv[2].split('n')[1]);
+                        //last file
                         if(i != process.argv.length-1)
                             process.stdout.write("\n");
                     }
                 }
             }
+            //if option is used without space between specifying line/byte
             else{
+                //single file
                 if(process.argv.length == 5){
                     printfilelines(process.argv[4], process.argv[3]);
                 }
+                //multiple file
                 else{
                     for(let i = 4;i < process.argv.length;i++){
                         console.log("==> "+ process.argv[i] +".txt <==")
                         printfilelines(process.argv[i], process.argv[3]);
+                        //last file
                         if(i != process.argv.length-1)
                             process.stdout.write("\n");
                     }
@@ -40,10 +48,13 @@ try{
         }
         //if option is with bytes
         else if(process.argv[2][1] == 'c'){
+            //if option is used with space between specifying line/byte
             if(process.argv[2].length > 2){
+                //single file
                 if(process.argv.length == 4){
                     printfilebytes(process.argv[3], process.argv[2].split('c')[1]);
                 }
+                //multiple file
                 else{
                     for(let i = 3;i < process.argv.length;i++){
                         console.log("==> "+ process.argv[i] +".txt <==")
@@ -53,10 +64,13 @@ try{
                     }
                 }
             }
+            //if option is used without space between specifying line/byte
             else{
+                //single file
                 if(process.argv.length == 5){
                     printfilebytes(process.argv[4], process.argv[3]);
                 }
+                //multiple file
                 else{
                     for(let i = 4;i < process.argv.length;i++){
                         console.log("==> "+ process.argv[i] +".txt <==")
@@ -69,9 +83,11 @@ try{
         }
         //if option is default line
         else{
+            //single file
             if(process.argv.length == 4){
                 printfilelines(process.argv[3], process.argv[2].split('-')[1]);
             }
+            //multiple file
             else{
                 for(let i = 3;i < process.argv.length;i++){
                     console.log("==> "+ process.argv[i] +".txt <==")
