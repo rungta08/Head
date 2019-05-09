7f71c68107b00448070cfb76538440cd07fccc75
Shivam.rungta <shivam.rungta@IN-Shivamrungta.local>
2019-05-09 14:21:48 +0530

    To show failed Test Case Added

--- test.sh
+++ test.sh
@@ -2,77 +2,149 @@
 # echo "Test Suit to print file content" >> result.txt
 #Tests for single file
 echo "Result Test case for command head filename" >> result.txt
-head inputfile1.txt >> expected.txt
-node head.js inputfile1 >> output.txt
+head inputfile1.txt > expected.txt
+node head.js inputfile1 > output.txt
 diff expected.txt output.txt >> result.txt
-echo "Completed" >> result.txt
+DIFF=$(diff expected.txt output.txt)
+if [ "$DIFF" ]
+then
+    echo "Test Case Failed" >> result.txt
+else
+    echo "Completed" >> result.txt
+fi
 
 echo "Result Test case for command head -n5 filename" >> result.txt
-head -n5 inputfile1.txt >> expected.txt
-node head.js -n5 inputfile1 >> output.txt
+head -n5 inputfile1.txt > expected.txt
+node head.js -n5 inputfile1 > output.txt
 diff expected.txt output.txt >> result.txt
-echo "Completed" >> result.txt
+DIFF=$(diff expected.txt output.txt)
+if [ "$DIFF" ]
+then
+    echo "Test Case Failed" >> result.txt
+else
+    echo "Completed" >> result.txt
+fi
 
 echo "Result Test case for command head -n 5 filename" >> result.txt
-head -n 5 inputfile1.txt >> expected.txt
-node head.js -n 5 inputfile1 >> output.txt
+head -n 5 inputfile1.txt > expected.txt
+node head.js -n 5 inputfile1 > output.txt
 diff expected.txt output.txt >> result.txt
-echo "Completed" >> result.txt
+DIFF=$(diff expected.txt output.txt)
+if [ "$DIFF" ]
+then
+    echo "Test Case Failed" >> result.txt
+else
+    echo "Completed" >> result.txt
+fi
 
 echo "Result Test case for command head -c5 filename" >> result.txt
-head -c5 inputfile1.txt >> expected.txt
-node head.js -c5 inputfile1 >> output.txt
+head -c5 inputfile1.txt > expected.txt
+node head.js -c5 inputfile1 > output.txt
 diff expected.txt output.txt >> result.txt
-echo "Completed" >> result.txt
+DIFF=$(diff expected.txt output.txt)
+if [ "$DIFF" ]
+then
+    echo "Test Case Failed" >> result.txt
+else
+    echo "Completed" >> result.txt
+fi
 
 echo "Result Test case for command head -c 5 filename" >> result.txt
-head -c 5 inputfile1.txt >> expected.txt
-node head.js -c 5 inputfile1 >> output.txt
+head -c 5 inputfile1.txt > expected.txt
+node head.js -c 5 inputfile1 > output.txt
 diff expected.txt output.txt >> result.txt
-echo "Completed" >> result.txt
+DIFF=$(diff expected.txt output.txt)
+if [ "$DIFF" ]
+then
+    echo "Test Case Failed" >> result.txt
+else
+    echo "Completed" >> result.txt
+fi
 
 echo "Result Test case for command head -5 filename" >> result.txt
-head -5 inputfile1.txt >> expected.txt
-node head.js -5 inputfile1 >> output.txt
+head -5 inputfile1.txt > expected.txt
+node head.js -5 inputfile1 > output.txt
 diff expected.txt output.txt >> result.txt
-echo "Completed" >> result.txt
+DIFF=$(diff expected.txt output.txt)
+if [ "$DIFF" ]
+then
+    echo "Test Case Failed" >> result.txt
+else
+    echo "Completed" >> result.txt
+fi
 
 
 #Test for multiple files
 echo "Result Test case for command head filename filename ..." >> result.txt
-head inputfile2.txt inputfile1.txt >> expected.txt
-node head.js inputfile2 inputfile1 >> output.txt
+head inputfile2.txt inputfile1.txt > expected.txt
+node head.js inputfile2 inputfile1 > output.txt
 diff expected.txt output.txt >> result.txt
-echo "Completed" >> result.txt
+DIFF=$(diff expected.txt output.txt)
+if [ "$DIFF" ]
+then
+    echo "Test Case Failed" >> result.txt
+else
+    echo "Completed" >> result.txt
+fi
 
 echo "Result Test case for command head filename -n5 filename ..." >> result.txt
-head -n5 inputfile2.txt inputfile1.txt >> expected.txt
-node head.js -n5 inputfile2 inputfile1 >> output.txt
+head -n5 inputfile2.txt inputfile1.txt > expected.txt
+node head.js -n5 inputfile2 inputfile1 > output.txt
 diff expected.txt output.txt >> result.txt
-echo "Completed" >> result.txt
+DIFF=$(diff expected.txt output.txt)
+if [ "$DIFF" ]
+then
+    echo "Test Case Failed" >> result.txt
+else
+    echo "Completed" >> result.txt
+fi
 
 echo "Result Test case for command head filename -n 5 filename ..." >> result.txt
-head -n 5 inputfile2.txt inputfile1.txt >> expected.txt
-node head.js -n 5 inputfile2 inputfile1 >> output.txt
+head -n 5 inputfile2.txt inputfile1.txt > expected.txt
+node head.js -n 5 inputfile2 inputfile1 > output.txt
 diff expected.txt output.txt >> result.txt
-echo "Completed" >> result.txt
+DIFF=$(diff expected.txt output.txt)
+if [ "$DIFF" ]
+then
+    echo "Test Case Failed" >> result.txt
+else
+    echo "Completed" >> result.txt
+fi
 
 echo "Result Test case for command head filename -c5 filename ..." >> result.txt
-head -c5 inputfile2.txt inputfile1.txt >> expected.txt
-node head.js -c5 inputfile2 inputfile1 >> output.txt
+head -c5 inputfile2.txt inputfile1.txt > expected.txt
+node head.js -c5 inputfile2 inputfile1 > output.txt
 diff expected.txt output.txt >> result.txt
-echo "Completed" >> result.txt
+DIFF=$(diff expected.txt output.txt)
+if [ "$DIFF" ]
+then
+    echo "Test Case Failed" >> result.txt
+else
+    echo "Completed" >> result.txt
+fi
 
 echo "Result Test case for command head filename -c 5 filename ..." >> result.txt
-head -c 5 inputfile2.txt inputfile1.txt >> expected.txt
-node head.js -c 5 inputfile2 inputfile1 >> output.txt
+head -c 5 inputfile2.txt inputfile1.txt > expected.txt
+node head.js -c 5 inputfile2 inputfile1 > output.txt
 diff expected.txt output.txt >> result.txt
-echo "Completed" >> result.txt
+DIFF=$(diff expected.txt output.txt)
+if [ "$DIFF" ]
+then
+    echo "Test Case Failed" >> result.txt
+else
+    echo "Completed" >> result.txt
+fi
 
 echo "Result Test case for command head filename -5 filename ..." >> result.txt
-head -5 inputfile2.txt inputfile1.txt >> expected.txt
-node head.js -5 inputfile2 inputfile1 >> output.txt
+head -5 inputfile2.txt inputfile1.txt > expected.txt
+node head.js -5 inputfile2 inputfile1 > output.txt
 diff expected.txt output.txt >> result.txt
-echo "Completed" >> result.txt
+DIFF=$(diff expected.txt output.txt)
+if [ "$DIFF" ]
+then
+    echo "Test Case Failed" >> result.txt
+else
+    echo "Completed" >> result.txt
+fi
 
 
